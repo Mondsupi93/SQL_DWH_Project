eba27c38f84ff15071de3aea7792cc641087e841
Erfan <96449075+Mondsupi93@users.noreply.github.com>
2026-06-07 22:45:20 +0200

    Update proc_load_bronze.sql

--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -1,88 +1,179 @@
-IF OBJECT_ID('bronze.crm_cust_info', 'U') IS NOT NULL 
-	TRUNCATE TABLE bronze.crm_cust_info
-GO
-
-BULK INSERT bronze.crm_cust_info
-FROM 'D:\Erfan Folder\Training\The complete SQL Bootcamp\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
-WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK
-);
-GO
-
-IF OBJECT_ID('bronze.crm_prd_info', 'U') IS NOT NULL 
-	TRUNCATE TABLE bronze.crm_prd_info
-GO
-
-BULK INSERT bronze.crm_prd_info
-FROM 'D:\Erfan Folder\Training\The complete SQL Bootcamp\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
-WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK
-);
-
-GO
-
-IF OBJECT_ID('bronze.crm_sales_details', 'U') IS NOT NULL 
-	TRUNCATE TABLE bronze.crm_sales_details
-GO
-
-BULK INSERT bronze.crm_sales_details
-FROM 'D:\Erfan Folder\Training\The complete SQL Bootcamp\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
-WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK
-);
-
-
---LOAD ERP Data into ERP Tables
-
-GO
-
-IF OBJECT_ID('bronze.erp_cust_az12', 'U') IS NOT NULL 
-	TRUNCATE TABLE bronze.erp_cust_az12
-GO
-
-BULK INSERT bronze.erp_cust_az12
-FROM 'D:\Erfan Folder\Training\The complete SQL Bootcamp\sql-data-warehouse-project\datasets\source_erp\CUST_AZ12.csv'
-WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK
-);
+--EXEC bronze.load_bronze
 
 
 
-GO
+CREATE OR ALTER PROCEDURE bronze.load_bronze AS 
 
-IF OBJECT_ID('bronze.erp_loc_a101', 'U') IS NOT NULL 
-	TRUNCATE TABLE bronze.erp_loc_a101
-GO
+BEGIN
 
-BULK INSERT bronze.erp_loc_a101
-FROM 'D:\Erfan Folder\Training\The complete SQL Bootcamp\sql-data-warehouse-project\datasets\source_erp\LOC_A101.csv'
-WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK
-);
+	BEGIN TRY
+		---------------------------------------------------------------------------
+		--Define Variables to setup the time of each query
+		DECLARE @start_time DATETIME, 
+				@end_time DATETIME, 
+				@batch_start_time DATETIME, 
+				@batch_end_time DATETIME;
+		---------------------------------------------------------------------------
+		SET @batch_start_time = GETDATE();
+		PRINT '====================================================';
+		PRINT 'Loading Bronze Layer';
+		PRINT '====================================================';
 
+		PRINT '----------------------------------------------------';
+		PRINT 'Loading CRM Table';
+		PRINT '-----------------------------------------------------';
 
+		---------------------------------------------------------------------------
 
+		SET @start_time = GETDATE();
+		PRINT '>>Truncating Table: bronze.crm_cust_info';
 
-GO
+		IF OBJECT_ID('bronze.crm_cust_info', 'U') IS NOT NULL 
+			TRUNCATE TABLE bronze.crm_cust_info;
+	
+		PRINT '>>Inserting Data into: bronze.crm_cust_info';
 
-IF OBJECT_ID('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL 
-	TRUNCATE TABLE bronze.erp_px_cat_g1v2
-GO
+		BULK INSERT bronze.crm_cust_info
+		FROM 'D:\Erfan Folder\Training\The complete SQL Bootcamp\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		SET @end_time = GETDATE();
+		PRINT '>> Load Duration:' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + 'second';
+		PRINT '-------------------------';
 
-BULK INSERT bronze.erp_px_cat_g1v2
-FROM 'D:\Erfan Folder\Training\The complete SQL Bootcamp\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
-WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK
-);
+		---------------------------------------------------------------------------
+
+		SET @start_time = GETDATE();
+		PRINT '>>Truncating Table: bronze.crm_prd_info';
+
+		IF OBJECT_ID('bronze.crm_prd_info', 'U') IS NOT NULL 
+			TRUNCATE TABLE bronze.crm_prd_info;
+	
+		PRINT '>>Inserting Data into: bronze.crm_prd_info';
+
+		BULK INSERT bronze.crm_prd_info
+		FROM 'D:\Erfan Folder\Training\The complete SQL Bootcamp\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		SET @end_time = GETDATE();
+		PRINT '>> Load Duration:' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + 'second';
+		PRINT '-------------------------';
+
+		---------------------------------------------------------------------------
+
+		SET @start_time = GETDATE();
+		PRINT '>>Truncating Table: bronze.crm_sales_details';
+
+		IF OBJECT_ID('bronze.crm_sales_details', 'U') IS NOT NULL 
+			TRUNCATE TABLE bronze.crm_sales_details;
+	
+
+		PRINT '>>Inserting Data into: bronze.crm_sales_details';
+
+		BULK INSERT bronze.crm_sales_details
+		FROM 'D:\Erfan Folder\Training\The complete SQL Bootcamp\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		SET @end_time = GETDATE();
+		PRINT '>> Load Duration:' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + 'second';
+		PRINT '-------------------------';
+
+		---------------------------------------------------------------------------
+
+		PRINT '----------------------------------------------------';
+		PRINT 'Loading ERP Table';
+		PRINT '-----------------------------------------------------';
+
+	
+		SET @start_time = GETDATE();
+		PRINT '>>Truncating Table: bronze.erp_cust_az12';
+
+		IF OBJECT_ID('bronze.erp_cust_az12', 'U') IS NOT NULL 
+			TRUNCATE TABLE bronze.erp_cust_az12;
+	
+
+		PRINT '>>Inserting Data into: bronze.erp_cust_az12';
+
+		BULK INSERT bronze.erp_cust_az12
+		FROM 'D:\Erfan Folder\Training\The complete SQL Bootcamp\sql-data-warehouse-project\datasets\source_erp\CUST_AZ12.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		SET @end_time = GETDATE();
+		PRINT '>> Load Duration:' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + 'second';
+		PRINT '-------------------------';
+
+		---------------------------------------------------------------------------
+
+		SET @start_time = GETDATE();
+		PRINT '>>Truncating Table: bronze.erp_loc_a101';
+
+		IF OBJECT_ID('bronze.erp_loc_a101', 'U') IS NOT NULL 
+			TRUNCATE TABLE bronze.erp_loc_a101;
+	
+
+		PRINT '>>Inserting Data into: bronze.erp_loc_a101';
+	
+		BULK INSERT bronze.erp_loc_a101
+		FROM 'D:\Erfan Folder\Training\The complete SQL Bootcamp\sql-data-warehouse-project\datasets\source_erp\LOC_A101.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		SET @end_time = GETDATE();
+		PRINT '>> Load Duration:' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + 'second';
+		PRINT '-------------------------';
+
+		---------------------------------------------------------------------------
+
+		SET @start_time = GETDATE();
+		PRINT '>>Truncating Table: bronze.erp_px_cat_g1v2';
+
+		IF OBJECT_ID('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL 
+			TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+	
+
+		PRINT '>>Inserting Data into: bronze.erp_px_cat_g1v2';
+	
+		BULK INSERT bronze.erp_px_cat_g1v2
+		FROM 'D:\Erfan Folder\Training\The complete SQL Bootcamp\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+
+		SET @end_time = GETDATE();
+		PRINT '>> Load Duration:' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + 'second';
+		PRINT '-------------------------';
+
+		
+		SET @batch_end_time = GETDATE();
+		PRINT '====================================================';
+		PRINT 'Loading Bronze Layer is Completed';
+		PRINT '    >>Total Load Duration:' + CAST(DATEDIFF(second, @batch_start_time, @batch_end_time) AS NVARCHAR) + 'second';
+		PRINT '====================================================';
+
+	
+	END TRY
+	BEGIN CATCH
+		PRINT '====================================================';
+		PRINT 'ERROR OCCURED DURING LOADING BRONZE LAYER';
+		PRINT 'ERROR MESSAGE' + ERROR_MESSAGE();
+		PRINT 'ERROR MESSAGE' + CAST(ERROR_NUMBER() AS NVARCHAR);
+		PRINT 'ERROR MESSAGE' + CAST(ERROR_STATE() AS NVARCHAR);
+		PRINT '====================================================';
+	END CATCH
+END
